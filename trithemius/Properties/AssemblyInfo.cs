077e127bf4a7f61b48c2c4c10f0601ee709cd50d
Tim Baxendale <pcluddite@outlook.com>
2026-04-28 23:29:11 -0500

    Add AssemblyInfo.cs to trithemius

---
+++ trithemius/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+// =====
+//
+// Copyright (c) 2013-2026 Timothy Baxendale
+//
+// =====
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyCopyright("Copyright (c) 2013-2026 Timothy Baxendale")]
+[assembly: AssemblyFileVersion("0.5.2.1")]
+[assembly: AssemblyInformationalVersion("0.5.2.1")]
+[assembly: AssemblyTitle("Trithemius")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7abe80c-8f19-45c2-bc05-cd71277e0379")]
